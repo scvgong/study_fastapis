4e10a11dad90a135a9997a44f2e0137275c63728
scvgong <gongmyeongyun@gmail.com>
2024-01-04 14:37:45 +0900

    quest

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ~$ pip install fastapi uvicon
 ~$ pip install python-multipart
 '''
+
+회원가입(post) -> 로그인(post) -> 회원리스트(get) -> 회원 상세 : 리스트(get), 삭제(post)
+function 내에 print : query or path parameters
--- routes/users.py
+++ routes/users.py
@@ -10,25 +10,41 @@ from fastapi import Request
 templates = Jinja2Templates(directory="templates/")
 
 # 회원가입 form /users/form -> users/inserts.html
-@router.get("/form") # 어노테이션(웹에서 업무(function) 호출)
-async def insert(request:Request):
+@router.get("/form") 
+async def form(request:Request):
     return templates.TemplateResponse(name="users/inserts.html",context={"request":request})
 
-# 회원가입 /users/insert -> users/logins.html
-@router.get("/inserts") # 어노테이션(웹에서 업무(function) 호출)
+# 회원가입(post 방식) /users/insert -> users/logins.html
+@router.post("/inserts")
 async def insert(request:Request):
+    dict_details = dict(await request.form())
+    print(dict_details)
     pass # business
     return templates.TemplateResponse(name="users/logins.html",context={"request":request})
 
-# 회원리스트 /users/lists -> users/lists.html
-@router.get("/lists") # 어노테이션(웹에서 업무(function) 호출)
+# 회원리스트(post 방식) /users/login -> users/lists.html
+@router.post("/lists") 
 async def list(request:Request):
+    dict_details = dict(await request.form())
+    print(dict_details)
+    pass # business
+    return templates.TemplateResponse(name="users/lists.html",context={"request":request})
+
+# 회원리스트(get 방식) /users/lists -> users/lists.html
+@router.get("/lists")
+async def list(request:Request):
+    dict_details = dict(await request.form())
+    print(dict_details)
     pass # business
     return templates.TemplateResponse(name="users/lists.html",context={"request":request})
 
 # 회원상세정보 /users/reads -> users/reads.html
 # Path parameters : /users/reads/id or /users/reads/uniqe_name
 @router.get("/reads/{object_id}") 
-async def list(request:Request,object_id):
+async def read(request:Request,object_id):
+    dict_details = dict(await request.form())
+    print(dict_details)
     pass # business
     return templates.TemplateResponse(name="users/reads.html",context={"request":request})
+
+#회원가입(post) -> 로그인(post) -> 회원리스트(get) -> 회원 상세 : 리스트(get), 삭제(post)
